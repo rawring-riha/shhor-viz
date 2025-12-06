675f52a6000430c2ccecfca11122af3cbcaaf2b6
Harikrishnan P <harikrishnan.p2551@gmail.com>
2025-12-07 04:26:01 +0530

    chore: Add github actions deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
+
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
-  base: '/shhor-viz/', // Replace with your actual repository name
+  base: '/shhor-viz/',
   build: {
-    outDir: 'dist'
-  }
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  },
+  publicDir: 'public'
 })
